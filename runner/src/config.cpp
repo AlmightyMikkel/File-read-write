e0bf1c78cc84dee0d119e1741b40303e73f6076f
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2022-10-04 13:11:56 +0200

    Update config.cpp
    Converted to unordered map

--- runner/src/config.cpp
+++ runner/src/config.cpp
@@ -1,10 +1,33 @@
 #include "config.h"
 
 bool Config::load(const std::string& filename) {
+	
+	size_t delimPos{};
+	std::string content;
+	
+	
 	config_stream.open(filename);
 	if (!config_stream.is_open()) {
 		return false;
 	} 
+	
+	while (!config_stream.eof()){
+		
+		Config_pair pair;
+		
+		std::getline(config_stream, content);
+		
+
+		delimPos = std::string.find(':');
+		pair.key = content.substr(0, delimPos);
+		pair.value = content.substr(delimPos + 1);
+		
+		config_container.insert({pair.key, pair.value});
+		
+		
+	}
+	
+	
 	return true;
 }
 
