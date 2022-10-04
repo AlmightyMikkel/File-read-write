7af783032488b50d6c908ec88839955ff4167b28
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2022-10-04 13:14:46 +0200

    Update config.cpp
    Implemented the three main functions

--- runner/src/config.cpp
+++ runner/src/config.cpp
@@ -39,20 +39,34 @@ bool Config::contains(const std::string& key) const
 }
 
 bool Config::as_int(const std::string& key, int& value) const {
-	int file_value;
+	
+	for (auto& pair : config_container){
+		if(pair.key == key){
+			value = std::stoi(pair.value);
+			return true;
+		}
+	}
 	
 	return false;
 }
 
 bool Config::as_float(const std::string& key, float& value) const {
-	float file_value;
-	
+	for (auto& pair : config_container){
+		if(pair.key == key){
+			value = std::stof(pair.value);
+			return true;
+		}
+	}
 	return false;
 }
 
 bool Config::as_string(const std::string& key, std::string& value) const {
-	std::string file_value;
-	
+	for (auto& pair : config_container){
+		if(pair.key == key){
+			value = spair.value;
+			return true;
+		}
+	}
 	return false;
 }
 
