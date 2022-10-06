b996a65ec629748da3373aa245dbbb819098d350
Mikkel Sørensen <kaktusdrengen@outlook.dk>
2022-10-06 19:42:54 +0200

    Add files via upload

---
+++ runner/include/config.h
@@ -0,0 +1,21 @@
+#include <fstream>
+#include <string>
+#include <unordered_map>
+
+
+class Config {
+public:
+	Config() = default;
+	bool load(const std::string& filename);
+	bool contains(const std::string& key) const;
+	bool as_int(const std::string& key, int& value) const;
+	bool as_float(const std::string& key, float& value) const;
+	bool as_string(const std::string& key, std::string& value) const;
+private:
+	std::ifstream config_stream;
+	std::unordered_map<std::string, std::string> config_container;
+<<<<<<< HEAD
+};
+=======
+};
+>>>>>>> c3179183b00178e17d69d8b5315913ff8c31b5af
